38c855fb4d8d61416cf53d13e221b7041255eece
Changbin Park <gh4ck3r@gmail.com>
2019-03-09 19:32:29 +0900

    Fix error for eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,7 +9,7 @@ module.exports = {
   },
     extends: 'eslint:recommended',
     parserOptions: {
-      sourceType: 'module'
+      sourceType: 'module',
       ecmaVersion: 8,
     },
     rules: {
